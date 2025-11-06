e06cc6847f1ed4076b062e58db2df208566b9da6
Maksim Korotenko <mskorotenko@mail.ru>
2025-11-06 17:14:46 +0300

    Task5

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        IMonsterHandler<Monster> handler = new MonsterHandler();
+
+        // Контравариантность позволяет это
+        IMonsterHandler<Zombie> zombieHandler = handler;
+        IMonsterHandler<Skillet> skilletHandler = handler;
+
+        zombieHandler.Handle(new Zombie());
+        skilletHandler.Handle(new Skillet());
+    }
+}
+
+public interface IMonsterHandler<in T>
+{
+    void Handle(T monster);
+}
+
+public class MonsterHandler : IMonsterHandler<Monster>
+{
+    public void Handle(Monster monster)
+    {
+        Console.WriteLine($"{monster.GetType().Name} получил урон!");
+    }
+}
+
+public class Monster { }
+
+public class Zombie : Monster { }
+
+public class Skillet : Monster { }
+
